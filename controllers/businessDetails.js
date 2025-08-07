7de4b16a5c15a0aa4560cfe14a44a8002c06545b
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-07 10:46:45 +0100

    Amended getBusinessList to set ukPropertyBusinessId in req.session.user

--- controllers/businessDetails.js
+++ controllers/businessDetails.js
@@ -10,8 +10,23 @@ const getBusinessList = asyncHandler(async (req, res) => {
 
   const apiResponse = await fetchBusinessListByNino({ req, nino });
 
+  // Set UK property business ID
+  req.session.user.ukPropertyBusinessId = getUkPropertyBusinessId(apiResponse.body);
+
   return res.status(apiResponse.status).json(apiResponse.body);
 });
+
+function getUkPropertyBusinessId(data) {
+  console.log('Received data:', data);
+
+  if (!data || !Array.isArray(data.listOfBusinesses)) return null;
+
+  const ukProperty = data.listOfBusinesses.find(
+    (business) => business.typeOfBusiness === 'uk-property'
+  );
+
+  return ukProperty ? ukProperty.businessId : null;
+}
 
 module.exports = {
   getBusinessList
