9f9d318ce9a6fd98b6cd5a66188369c4daeea395
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-10 11:34:15 +0100

    Added repo file to handle api calls

---
+++ repositories/businessDetailsRepo.js
@@ -0,0 +1,29 @@
+const utils = require('../utils');
+
+const businessDetailsServices = {
+  businessDetails: {
+    name: 'individuals/business/details',
+    version: '1.0',
+    routes: {
+      listByNino: (nino) => `/${encodeURIComponent(nino)}/list`
+    }
+  }
+};
+
+async function fetchBusinessDetailsList({ req, nino }) {
+  const accessToken = await utils.getUserRestrictedToken(req);
+  const routePath = businessDetailsServices.businessDetails.routes.listByNino(nino);
+
+  const response = await utils.callApi({
+    method: 'GET',
+    serviceName: businessDetailsServices.businessDetails.name,
+    serviceVersion: businessDetailsServices.businessDetails.version,
+    routePath,
+    bearerToken: accessToken,
+    extraHeaders: { 'Gov-Test-Scenario': 'STATEFUL' }
+  });
+
+  return response;
+};
+
+module.exports = { fetchBusinessDetailsList }
