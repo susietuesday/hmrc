55ec211c5cdc4fccc4358e4f681f97cb3328837d
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-16 07:10:24 +0100

    Amended to use DYNAMIC test scenario

--- services/obligationsService.js
+++ services/obligationsService.js
@@ -6,7 +6,10 @@ async function getIncomeAndExpenditureObligations(nino, req) {
   const params = {
     typeOfBusiness: 'uk-property',
     //businessId: req.session.user.ukPropertyBusinessId
-    businessId: 'XPIS12345678903'
+    businessId: 'XPIS12345678901',
+    fromDate: '2025-04-06',
+    toDate: '2025-07-05'
+    //status: ''
   }
 
   const response = await obligationsRepo.fetchIncomeAndExpenditureObligations(nino, params, req); 
