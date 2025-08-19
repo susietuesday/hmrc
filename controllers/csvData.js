3ffa073dc5ff8268ca0e8874c51c1f3a8f6aa0cc
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-19 23:13:54 +0100

    Added uploadCsvExpensesFile

--- controllers/csvData.js
+++ controllers/csvData.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-const { processCsvIncomeFile } = require('../services/csvDataService');
+const { processCsvIncomeFile, processCsvExpensesFile } = require('../services/csvDataService');
 const utils = require('../utils/utils');
 
 const uploadCsvIncomeFile = asyncHandler(async (req, res) => {
@@ -23,6 +23,28 @@ const uploadCsvIncomeFile = asyncHandler(async (req, res) => {
   });
 });
 
+const uploadCsvExpensesFile = asyncHandler(async (req, res) => {
+  // Check if file is provided
+  if (!req.file) {
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+
+  const { results, totalAmount } = await processCsvExpensesFile(req.file.buffer);
+
+  // Save cumulative totals to session
+  req.session.user.summary.fromDate = utils.getCurrentTaxYearStart();
+  req.session.user.summary.toDate = utils.getTodayDate();
+  req.session.user.summary.ukProperty.expenses.consolidatedExpenses = totalAmount;
+
+  res.json({
+    message: 'CSV parsed successfully',
+    data: {
+      results
+    }
+  });
+});
+
 module.exports = {
-  uploadCsvIncomeFile
+  uploadCsvIncomeFile,
+  uploadCsvExpensesFile
 };
