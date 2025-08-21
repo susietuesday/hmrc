2a9417c41dd9afd268376fbf698c745e5e63e7be
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-21 22:06:20 +0100

    Amended to set fromDate, toDate and defaultToDate

--- controllers/summary.js
+++ controllers/summary.js
@@ -1,11 +1,20 @@
 const asyncHandler = require('express-async-handler');
+const utils = require('../utils/utils');
 
 const showSummaryPage = asyncHandler(async(req, res) => {
   const summary = req.session.user.summary.ukProperty;
 
+  // Set default dates
+  req.session.user.summary.fromDate = utils.getCurrentTaxYearStart();
+  req.session.user.summary.toDate = utils.getTodayDate();
+  const defaultToDate = utils.addDays(summary.toDate, 10);
+
   res.render("summary", {
     income: summary.income,
-    expenses: summary.expenses
+    expenses: summary.expenses,
+    fromDate: summary.fromDate,
+    toDate: summary.toDate,
+    defaultToDate: defaultToDate
   });
 });
 
