bbf6b2d296c081f0995b498a1d65218a50195023
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-22 13:12:25 +0100

    Added createUkPropertyCumulativeSummary

--- repositories/propertyBusinessRepo.js
+++ repositories/propertyBusinessRepo.js
@@ -6,6 +6,7 @@ const services = {
     version: '6.0',
     routes: {
       createUkPropertyPeriodSummary: (nino, businessId, taxYear) => `/uk/${encodeURIComponent(nino)}/${encodeURIComponent(businessId)}/period/${taxYear}`,
+      createUkPropertyCumulativeSummary: (nino, businessId, taxYear) => `/uk/${encodeURIComponent(nino)}/${encodeURIComponent(businessId)}/cumulative/${taxYear}`,
       fetchUkPropertyCumulativeSummary: (nino, businessId, taxYear) => `/uk/${encodeURIComponent(nino)}/${encodeURIComponent(businessId)}/cumulative/${taxYear}`
     }
   }
@@ -24,6 +25,30 @@ async function createUkPropertyPeriodSummary({ nino, businessId, taxYear, body, 
 
   const response = await apiUtils.callApi({
     method: 'POST',
+    serviceName: services.propertyBusiness.name,
+    serviceVersion: services.propertyBusiness.version,
+    routePath,
+    bearerToken: accessToken,
+    body,
+    extraHeaders
+  });
+
+  return response;
+}
+
+async function createUkPropertyCumulativeSummary({ nino, businessId, taxYear, body, context }) {
+  const fraudHeaders = apiUtils.getFraudPreventionHeaders(context);
+  const accessToken = await apiUtils.getUserRestrictedToken(context.oauth2Token);
+  
+  const routePath = services.propertyBusiness.routes.createUkPropertyCumulativeSummary(nino, businessId, taxYear)
+
+  const extraHeaders = {
+    'Gov-Test-Scenario': 'STATEFUL',
+    ...fraudHeaders,
+  };
+
+  const response = await apiUtils.callApi({
+    method: 'PUT',
     serviceName: services.propertyBusiness.name,
     serviceVersion: services.propertyBusiness.version,
     routePath,
@@ -55,5 +80,6 @@ async function fetchUkPropertyCummulativeSummary({ nino, businessId, taxYear, se
 
 module.exports = {
   createUkPropertyPeriodSummary,
+  createUkPropertyCumulativeSummary,
   fetchUkPropertyCummulativeSummary
 };
