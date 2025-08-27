7d8653eb00c94aa15ffa291d0c6e270eb5bee524
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-27 09:01:53 +0100

    Deleted postUkPropertyPeriodSummary function

--- controllers/propertyBusiness.js
+++ controllers/propertyBusiness.js
@@ -1,25 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const propertyBusinessService = require('../services/propertyBusinessService.js');
 
-const postUkPropertyPeriodSummary = asyncHandler(async (req, res) => { 
-  const nino = req.query.nino;
-  const businessId = 'XBIS12345678901'
-  const taxYear = '2024-25'
-
-  if (!nino) {
-    return res.status(400).send('NINO is required');
-  }
-  
-  const apiResponse = await propertyBusinessService.createUkPropertyPeriodSummary({
-    nino, 
-    businessId, 
-    taxYear,
-    session: req.session
-  });
-
-  return res.status(apiResponse.status).json(apiResponse.body);
-});
-
 const getUkPropertyCumulativeSummary = asyncHandler(async (req, res) => {
   const nino = req.query.nino;
   const businessId = req.session.user.ukPropertyBusinessId;
@@ -40,6 +21,5 @@ const getUkPropertyCumulativeSummary = asyncHandler(async (req, res) => {
 });
 
 module.exports = { 
-  postUkPropertyPeriodSummary,
   getUkPropertyCumulativeSummary
 };
