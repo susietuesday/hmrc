8f1a012e0b06d4c62bf249a529db8c98c90c3ca0
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-27 09:02:20 +0100

    Deleted createUkPropertyPeriodSummary function

--- services/propertyBusinessService.js
+++ services/propertyBusinessService.js
@@ -1,57 +1,6 @@
 const dataUtils = require('../utils/dataUtils.js');
 const propertyBusinessRepo = require('../repositories/propertyBusinessRepo.js');
 
-async function createUkPropertyPeriodSummary({ nino, businessId, taxYear, session }) {
-  const data = {
-    "fromDate": "2024-04-06",
-    "toDate": "2024-07-05",
-    "ukFhlProperty": {
-        "income": {
-        "periodAmount": 5000.99,
-        "taxDeducted": 3123.21,
-        "rentARoom": {
-            "rentsReceived": 532.12
-        }
-        },
-        "expenses": {
-        "consolidatedExpenses": 988.18,
-        "rentARoom": {
-            "amountClaimed": 3000.87
-        }
-        }
-    },
-    "ukNonFhlProperty": {
-        "income": {
-        "premiumsOfLeaseGrant": 42.12,
-        "reversePremiums": 84.31,
-        "periodAmount": 9884.93,
-        "taxDeducted": 842.99,
-        "otherIncome": 31.44,
-        "rentARoom": {
-            "rentsReceived": 947.66
-        }
-        },
-        "expenses": {
-        "consolidatedExpenses": 988.18,
-        "residentialFinancialCost": 988.18,
-        "residentialFinancialCostsCarriedForward": 302.23,
-        "rentARoom": {
-            "amountClaimed": 952.53
-        }
-        }
-    }
-  }
-
-  const response = await propertyBusinessRepo.createUkPropertyPeriodSummary({
-    nino,
-    businessId,
-    taxYear,
-    body: data,
-    session
-  });
-  return response;
-}
-
 async function createUkPropertyCumulativeSummary({ nino, businessId, taxYear, body, context }) {
   const income = body.ukProperty.income;
   const expenses = body.ukProperty.expenses;
@@ -90,7 +39,6 @@ const data = {
   }
 };
 
-
   const response = await propertyBusinessRepo.createUkPropertyCumulativeSummary({
     nino,
     businessId,
@@ -113,7 +61,6 @@ async function getUkPropertyCumulativeSummary({ nino, businessId, taxYear, sessi
 }
 
 module.exports = {
-  createUkPropertyPeriodSummary,
   getUkPropertyCumulativeSummary,
   createUkPropertyCumulativeSummary
 };
