1dce12e7f5a1d1bfe3a6bd6a317352656bf13906
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-08-27 09:01:29 +0100

    Deleted period-summary route

--- routes.js
+++ routes.js
@@ -42,7 +42,6 @@ router.get('/business-sources', requireUser, getBusinessList);
 router.get('/obligations', requireUser, getObligations);
 router.post('/property-income', upload.single('csv'), csvData.uploadCsvIncomeFile);
 router.post('/property-expenses', upload.single('csv'), csvData.uploadCsvExpensesFile);
-router.post('/periodic-summary', requireUser, postUkPropertyPeriodSummary);
 router.get('/cumulative-summary', requireUser, getUkPropertyCumulativeSummary);
 router.get('/income', income.showIncomePage);
 router.get('/expenses', expenses.showExpensesPage);
