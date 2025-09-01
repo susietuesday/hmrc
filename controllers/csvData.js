506cb0d6039b4149654b6550b814c1d07623890f
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-09-01 09:32:21 +0100

    Mapped ukProperty to user-facing values for front end

--- controllers/csvData.js
+++ controllers/csvData.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const { processCsvIncomeFile, processCsvExpensesFile, extractTotalsFromBuffer } = require('../services/csvDataService');
+const { INCOME_CATEGORIES, EXPENSE_CATEGORIES} = require('../config/schemaMappings.js')
 
 const uploadCsvSummaryFile = asyncHandler(async (req, res) => {
   // Check if file is provided
@@ -8,17 +9,51 @@ const uploadCsvSummaryFile = asyncHandler(async (req, res) => {
   }
 
   const { ukProperty } = await extractTotalsFromBuffer(req.file.buffer);
+  console.log(JSON.stringify(ukProperty, null, 2));
 
   // Save cumulative totals to session
   req.session.user.summary.ukProperty = ukProperty;
 
+  const mappedUkProperty = convertKeysToCategories(ukProperty);
+  console.log(JSON.stringify(mappedUkProperty, null, 2));
+
   res.json({
     message: 'CSV parsed successfully',
     data: {
-      ukProperty
+      mappedUkProperty
     }
   });
 });
+
+function convertKeysToCategories(data) {
+  const result = { income: {}, expenses: {} };
+
+  // Helper: create lookup objects for faster access
+  const incomeLookup = Object.fromEntries(INCOME_CATEGORIES.map(({ key, category }) => [key, category]));
+  const expenseLookup = Object.fromEntries(EXPENSE_CATEGORIES.map(({ key, category }) => [key, category]));
+
+  // Convert income keys
+  for (const [key, value] of Object.entries(data.income)) {
+    if (typeof value === 'object' && key === 'rentARoom') {
+      result.income[incomeLookup['rentARoom.rentsReceived']] = value.rentsReceived;
+    } else {
+      const category = incomeLookup[key] || key;
+      result.income[category] = value;
+    }
+  }
+
+  // Convert expense keys
+  for (const [key, value] of Object.entries(data.expenses)) {
+    if (typeof value === 'object' && key === 'rentARoom') {
+      result.expenses[expenseLookup['rentARoom.amountClaimed']] = value.amountClaimed;
+    } else {
+      const category = expenseLookup[key] || key;
+      result.expenses[category] = value;
+    }
+  }
+
+  return result;
+}
 
 const uploadCsvIncomeFile = asyncHandler(async (req, res) => {
   // Check if file is provided
