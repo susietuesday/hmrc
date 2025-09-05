b663a45f4551786a3f009ec41a4d80dc6555877a
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-09-05 12:35:41 +0100

    Amended to put page handling events back into page code module

--- public/annualPage.js
+++ public/annualPage.js
@@ -1,6 +1,30 @@
-import { uploadCsv } from './utils/csvData.js';
+import { enableUploadButton, uploadCsv } from './utils/csvData.js';
 
 // Wait until the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
-  uploadCsv();
+  // --- Upload button handler ---
+  const fileInput = document.getElementById('csvFile');
+  const uploadBtn = document.getElementById('uploadBtn');
+  
+  enableUploadButton(fileInput, uploadBtn);
+
+  // Wait for upload button to be cicked
+  uploadBtn.addEventListener('click', async () => {
+
+    const formData = uploadCsv(fileInput);
+
+    const res = await fetch('/property-annual', {
+      method: 'POST',
+      body: formData
+    });
+
+    if (!res.ok) throw new Error('Upload failed');
+
+    const json = await res.json();
+
+    document.getElementById('uploadStatus').textContent = 'Upload successful!';
+
+    // Display the transaction rows
+    const previewDiv = document.getElementById('csvPreview');
+  });
 });
