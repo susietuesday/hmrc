c7db762e89d232b40ba16221681ca33b5360e9ae
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-09-09 21:24:24 +0100

    Added getCell, addValue and readBuffer functions

--- utils/dataUtils.js
+++ utils/dataUtils.js
@@ -1,5 +1,6 @@
 const streamifier = require('streamifier');
 const csv = require('csv-parser');
+const { Readable } = require('stream');
 
 // This function processes a CSV file buffer and extracts required columns.
 async function parseCsvBuffer(buffer, requiredColumns) {
@@ -36,8 +37,56 @@ function optionalNested(valueObj, key) {
   return { [key]: valueObj };
 }
 
+// Returns row and column indexes for specified spreadsheet cell
+function getCell(data, ref) {
+  const col = ref.match(/[A-Z]+/)[0];
+  const row = parseInt(ref.match(/\d+/)[0]) - 1;
+  const colIndex = col.split('').reduce((acc, char) => acc * 26 + (char.charCodeAt(0) - 64), 0) - 1;
+  return data[row][colIndex];
+}
+
+// Helper: only add the key if value is not null/empty
+function addValue(obj, key, value) {
+  // Convert value to string only if it exists
+  const strValue = (typeof value === 'string') ? value.toUpperCase() : '';
+
+  if (
+    value !== undefined &&
+    value !== null &&
+    value !== '' &&
+    strValue !== 'NOT APPLICABLE'
+  ) {
+    const keys = key.split('.');
+    let current = obj;
+
+    for (let i = 0; i < keys.length - 1; i++) {
+      if (!current[keys[i]]) {
+        current[keys[i]] = {}; // Create nested object if missing
+      }
+      current = current[keys[i]];
+    }
+
+    current[keys[keys.length - 1]] = value;
+  }
+}
+
+async function readBuffer(buffer) {
+  const results = [];
+
+  const stream = Readable.from(buffer.toString()).pipe(csv({ headers: false }));
+
+  for await (const row of stream) {
+    results.push(row);
+  }
+
+  return results;
+};
+
 module.exports = { 
   parseCsvBuffer,
   optionalProp,
-  optionalNested
+  optionalNested,
+  getCell,
+  addValue,
+  readBuffer
 };
