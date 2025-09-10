2ca45b0d7d3f2f5a6e404bc9aaeecf8acf48c659
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-09-10 15:53:56 +0100

    Corrected tabulation

--- services/propertyBusinessService.js
+++ services/propertyBusinessService.js
@@ -5,39 +5,39 @@ async function createUkPropertyCumulativeSummary({ nino, businessId, taxYear, bo
   const income = body.ukProperty.income;
   const expenses = body.ukProperty.expenses;
 
-const data = {
-  fromDate: body.fromDate,
-  toDate: body.toDate,
-  ukProperty: {
-    income: {
-      ...dataUtils.optionalProp(income.premiumsOfLeaseGrant, 'premiumsOfLeaseGrant'),
-      ...dataUtils.optionalProp(income.reversePremiums, 'reversePremiums'),
-      ...dataUtils.optionalProp(income.periodAmount, 'periodAmount'),
-      ...dataUtils.optionalProp(income.taxDeducted, 'taxDeducted'),
-      ...dataUtils.optionalProp(income.otherIncome, 'otherIncome'),
-      ...dataUtils.optionalNested(
-        income.rentARoom?.rentsReceived ? { rentsReceived: income.rentARoom.rentsReceived } : null,
-        'rentARoom'
-      )
-    },
-    expenses: {
-      ...dataUtils.optionalProp(expenses.premisesRunningCosts, 'premisesRunningCosts'),
-      ...dataUtils.optionalProp(expenses.repairsAndMaintenance, 'repairsAndMaintenance'),
-      ...dataUtils.optionalProp(expenses.financialCosts, 'financialCosts'),
-      ...dataUtils.optionalProp(expenses.professionalFees, 'professionalFees'),
-      ...dataUtils.optionalProp(expenses.costOfServices, 'costOfServices'),
-      ...dataUtils.optionalProp(expenses.other, 'other'),
-      ...dataUtils.optionalProp(expenses.residentialFinancialCost, 'residentialFinancialCost'),
-      ...dataUtils.optionalProp(expenses.travelCosts, 'travelCosts'),
-      ...dataUtils.optionalProp(expenses.residentialFinancialCostsCarriedForward, 'residentialFinancialCostsCarriedForward'),
-      ...dataUtils.optionalNested(
-        expenses.rentARoom?.amountClaimed ? { amountClaimed: expenses.rentARoom.amountClaimed } : null,
-        'rentARoom'
-      ),
-      ...dataUtils.optionalProp(expenses.consolidatedExpenses, 'consolidatedExpenses')
+  const data = {
+    fromDate: body.fromDate,
+    toDate: body.toDate,
+    ukProperty: {
+      income: {
+        ...dataUtils.optionalProp(income.premiumsOfLeaseGrant, 'premiumsOfLeaseGrant'),
+        ...dataUtils.optionalProp(income.reversePremiums, 'reversePremiums'),
+        ...dataUtils.optionalProp(income.periodAmount, 'periodAmount'),
+        ...dataUtils.optionalProp(income.taxDeducted, 'taxDeducted'),
+        ...dataUtils.optionalProp(income.otherIncome, 'otherIncome'),
+        ...dataUtils.optionalNested(
+          income.rentARoom?.rentsReceived ? { rentsReceived: income.rentARoom.rentsReceived } : null,
+          'rentARoom'
+        )
+      },
+      expenses: {
+        ...dataUtils.optionalProp(expenses.premisesRunningCosts, 'premisesRunningCosts'),
+        ...dataUtils.optionalProp(expenses.repairsAndMaintenance, 'repairsAndMaintenance'),
+        ...dataUtils.optionalProp(expenses.financialCosts, 'financialCosts'),
+        ...dataUtils.optionalProp(expenses.professionalFees, 'professionalFees'),
+        ...dataUtils.optionalProp(expenses.costOfServices, 'costOfServices'),
+        ...dataUtils.optionalProp(expenses.other, 'other'),
+        ...dataUtils.optionalProp(expenses.residentialFinancialCost, 'residentialFinancialCost'),
+        ...dataUtils.optionalProp(expenses.travelCosts, 'travelCosts'),
+        ...dataUtils.optionalProp(expenses.residentialFinancialCostsCarriedForward, 'residentialFinancialCostsCarriedForward'),
+        ...dataUtils.optionalNested(
+          expenses.rentARoom?.amountClaimed ? { amountClaimed: expenses.rentARoom.amountClaimed } : null,
+          'rentARoom'
+        ),
+        ...dataUtils.optionalProp(expenses.consolidatedExpenses, 'consolidatedExpenses')
+      }
     }
-  }
-};
+  };
 
   const response = await propertyBusinessRepo.createUkPropertyCumulativeSummary({
     nino,
@@ -46,8 +46,9 @@ const data = {
     body: data,
     context
   });
+  
   return response;
-}
+};
 
 async function getUkPropertyCumulativeSummary({ nino, businessId, taxYear, context }) {
 
