8b4802ff73259c9ef1aac9981f97f3a410c4e5eb
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-09-10 15:55:33 +0100

    Deleted uploadCsvIncomeFile and uploadCsvExpensesFile

--- controllers/csvData.js
+++ controllers/csvData.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-const { processCsvIncomeFile, processCsvExpensesFile, extractCsvAnnualData, extractCsvQuarterlyData } = require('../services/csvDataService');
+const { extractCsvAnnualData, extractCsvQuarterlyData } = require('../services/csvDataService');
 const { INCOME_CATEGORIES, EXPENSE_CATEGORIES} = require('../config/schemaMappings.js')
 
 const uploadCsvSummaryFile = asyncHandler(async (req, res) => {
@@ -73,47 +73,7 @@ function convertKeysToCategories(data) {
   return result;
 }
 
-const uploadCsvIncomeFile = asyncHandler(async (req, res) => {
-  // Check if file is provided
-  if (!req.file) {
-    return res.status(400).json({ message: 'No file uploaded' });
-  }
-
-  const { results, income } = await processCsvIncomeFile(req.file.buffer);
-
-  // Save cumulative totals to session
-  req.session.user.summary.ukProperty.income = income;
-
-  res.json({
-    message: 'CSV parsed successfully',
-    data: {
-      results
-    }
-  });
-});
-
-const uploadCsvExpensesFile = asyncHandler(async (req, res) => {
-  // Check if file is provided
-  if (!req.file) {
-    return res.status(400).json({ message: 'No file uploaded' });
-  }
-
-  const { results, expenses } = await processCsvExpensesFile(req.file.buffer);
-
-  // Save cumulative totals to session
-  req.session.user.summary.ukProperty.expenses = expenses;
-
-  res.json({
-    message: 'CSV parsed successfully',
-    data: {
-      results
-    }
-  });
-});
-
 module.exports = {
-  uploadCsvIncomeFile,
-  uploadCsvExpensesFile,
   uploadCsvSummaryFile,
   uploadCsvAnnualFile
 };
