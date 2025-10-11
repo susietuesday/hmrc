6dadae6a995a997be06f77212753057e75f50631
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-10-11 10:25:29 +0100

    Renamed constraints according to naming convention

--- prisma/migrations/0001_constraints.sql
+++ prisma/migrations/0001_constraints.sql
@@ -1,22 +1,20 @@
 -- Drop existing constraints if they exist
 ALTER TABLE IF EXISTS public.property_submissions
-  DROP CONSTRAINT IF EXISTS uk_property_period_submissions_pkey,
-  DROP CONSTRAINT IF EXISTS user_email_lowercase,
-  DROP CONSTRAINT IF EXISTS business_id,
-  DROP CONSTRAINT IF EXISTS nino,
-  DROP CONSTRAINT IF EXISTS tax_year;
+  DROP CONSTRAINT IF EXISTS prop_subs_pkey,
+  DROP CONSTRAINT IF EXISTS prop_subs_business_id_check,
+  DROP CONSTRAINT IF EXISTS prop_subs_nino_check,
+  DROP CONSTRAINT IF EXISTS prop_subs_tax_year_check;
 
 -- Recreate all constraints cleanly
 ALTER TABLE public.property_submissions
-  ADD CONSTRAINT uk_property_period_submissions_pkey PRIMARY KEY (id),
-  ADD CONSTRAINT user_email_lowercase CHECK (email = lower(email)),
-  ADD CONSTRAINT business_id CHECK (
+  ADD CONSTRAINT prop_subs_pkey PRIMARY KEY (id),
+  ADD CONSTRAINT prop_subs_business_id_check CHECK (
     business_id::text ~ '^X[A-Z0-9]{1}IS[0-9]{11}$'
   ) NOT VALID,
-  ADD CONSTRAINT nino CHECK (
+  ADD CONSTRAINT prop_subs_nino_check CHECK (
     nino::text ~ '^[A-CEGHJ-PR-TW-Z]{2}[0-9]{6}[A-D]$'
   ) NOT VALID,
-  ADD CONSTRAINT tax_year CHECK (
+  ADD CONSTRAINT prop_subs_tax_year_check CHECK (
     tax_year::text ~ '^(202[2-9])-([0-9]{2})$'
     AND (SUBSTRING(tax_year FROM 1 FOR 4)::integer + 1)
         = (('20' || SUBSTRING(tax_year FROM 6 FOR 2))::integer)
