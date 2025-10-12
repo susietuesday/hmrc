4dd5e66548f8b0479c1e1b3dc7bba257abd8d335
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-10-12 21:42:25 +0100

    Repository for PropertySubmission table

---
+++ repositories/propertyBusinessRepo.js
@@ -0,0 +1,20 @@
+const prisma = require('../lib/prismaClient');
+
+async function createPropertySubmission(nino, businessId, taxYear, body, response) {
+  const submission = await prisma.propertySubmission.create({
+    data: {
+      request_body: body,                // store body JSON directly
+      response_code: response?.status,   // or 200 if you want fixed
+      response_body: response?.data,     // store API response JSON directly
+      nino,                              // must match length 9
+      business_id: businessId,           // max length 20
+      tax_year: taxYear,                 // length 7
+    },
+  });
+
+  console.log("New submission created:", submission);
+};
+
+module.exports = {
+  createPropertySubmission
+};
