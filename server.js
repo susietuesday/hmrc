623a6bf2152847ec2c78deb7594767a7b5d98f16
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-10-12 21:39:17 +0100

    Added code to start server and handle graceful shutdown

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('./app');
+const { log } = require('./utils/utils');
+const { prisma } = require('./lib/prismaClient');
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+  log.info(`🚀 Server running on http://localhost:${PORT}`);
+  log.info('NODE_ENV =', process.env.NODE_ENV);
+});
+
+process.on('SIGTERM', async () => {
+  shutdown('SIGTERM');
+});
+
+process.on('SIGINT', async () => {
+  shutdown('SIGINT');
+});
+
+async function shutdown(signal) {
+  log.info('🛑 ' + signal + ' received. Closing server...');
+  await prisma.$disconnect();
+  server.close(() => {
+    log.info('Server closed.');
+    process.exit(0);
+  });
+}
