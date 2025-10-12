e982efc203959b2e57e535bab2ba33973e127018
Susie Bradshaw <susie_bradshaw@hotmail.com>
2025-10-12 21:40:34 +0100

    Removed code to add submission to database

--- clients/propertyBusinessClient.js
+++ clients/propertyBusinessClient.js
@@ -1,7 +1,3 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-console.log("Prisma loaded");
-
 const apiUtils = require('../utils/apiUtils.js');
 
 const services = {
@@ -36,35 +32,8 @@ async function createUkPropertyCumulativeSummary({ nino, businessId, taxYear, bo
     extraHeaders
   });
 
-  createUkPropertyPeriodSubmission(nino, businessId, taxYear, body, response)
-    .catch((e) => {
-      console.error("Error creating submission:", e);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
-
   return response;
 }
-
-async function createUkPropertyPeriodSubmission(nino, businessId, taxYear, body, response) {
-  const submission = await prisma.uk_property_period_submissions.create({
-    data: {
-      request_body: {
-        body
-      },
-      response_code: 200, // optional
-      response_body: {
-        response
-      },
-      nino: nino, // must match length 9
-      business_id: businessId, // max length 20
-      tax_year: taxYear, // length 7
-    },
-  });
-
-  console.log("New submission created:", submission);
-};
 
 async function fetchUkPropertyCummulativeSummary({ nino, businessId, taxYear, context }) {
   const fraudHeaders = apiUtils.getFraudPreventionHeaders(context);
